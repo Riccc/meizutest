25f8d6d893d592e28989d53e8653c0c05050618c
Ric <tangminpeng@tangminpengdeMacBook-Pro.local>
2017-08-30 02:10:24 +0800

    ric second commit

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,27 +1,31 @@
 class TagsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => [:change_tag_name,:change_photo_info]
   def index
     @datas = Tag.all
+    @photos = Photo.where(:tag_id => params[:id]).page(params[:page]).per(6)
     @photo = Photo.new
+    if params[:id].present?
+      @tag_name = Tag.find_by_id(params[:id]).name
+    else
+      @tag_name = ""
+    end
   end
 
   def create_photo
     @photo = Photo.create(photo_params)
     @tag_name = Tag.find_by_id(@photo.tag_id).name
-    respond_to do |format|
-      format.js
-    end
+        redirect_to({:action=>"index"})
   end
 
-  def get_photos
-    tag_data = Tag.find_by_name(params[:tag_name])
-    photos_data = Photo.where(:tag_id=>tag_data.id).page(params[:page]).per(3)
-    binding.pry
-    photo_url = []
-    photos_data.each do |d|
-      photo_url.push(d.avatar.url)
-    end
-    info = {:tag_info=>photos_data,:tag=>tag_data.name,:tag_url=>photo_url}
-    render :json=>info
+  def change_tag_name
+    tag = Tag.find_by_name(params[:old_tag_name])
+    tag.update_attribute(:name,params[:new_tag_name])
+  end
+
+  def change_photo_info
+    photo = Photo.find_by_id(params[:photo_info])
+    tag = Tag.find_by_name(params[:tag_info])
+    photo.update_attribute(:tag_id,tag.id)
   end
 
   private
